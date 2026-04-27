0d37a8e3650ff6f945fd7edb5ddd82d7197a7f89
Ngaruka Daniel <ngarukadaniel122@gmail.com>
2026-04-27 10:00:37 +0200

    js exercise

---
+++ js Exercise/2D array sort and sum/index.js
@@ -0,0 +1,27 @@
+function sum(data){
+    for(let i=0;i<data.length;i++){
+        let d = data[i]
+        for(let j=0; j<d.length;j++){
+            for(let k=j+1;k<d.length;k++){
+                if(d[j]>d[k]){
+                    let temp = d[j]
+                    d[j] = d[k]
+                    d[k]=temp
+                }
+            }
+        }
+        console.log(d);
+    }
+    let sum = 0
+    for(let i=0; i<data.length;i++){
+        sum += data[i][0]
+    }
+    return sum    
+}
+
+let data = [ [ 5, 3, 2, 4, 1 ]        //  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        // minimum value of row is 5
+, [ 21, 20, 14, 156, 100 ]  // minimum value of row is 20
+]
+
+console.log(sum(data))
--- js Exercise/filterPrinNumber/index.js
+++ js Exercise/filterPrinNumber/index.js
@@ -5,7 +5,7 @@ function filterPrimeNumbers(num){
 
         let count = 0;
         let cnum = num[i]
-
+        if(cnum<2)continue;
 
         for(let j = 1; j<= cnum; j++){
             if(cnum % j === 0) count++;
@@ -14,12 +14,13 @@ function filterPrimeNumbers(num){
 
         if(count===2) arr.push(cnum) 
     }
-
+    
     return arr
 
 }
 
-let a = [4, 7, 12, 11, 15, 17, 20, 23]
+
+let a = [4, -7, 12, -11, 15, 17, 20, 23]
 let b = [1, 2, 3, 4, 5, 6]
 let c = [91, 92, 93, 97, 98, 99]
 
